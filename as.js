2ddc9d64d8517c04d25940a8ced04990cf7e82ff
Puryash <133904166+Puryash@users.noreply.github.com>
2023-05-22 21:31:34 +0530

    llll

---
+++ as.js
@@ -0,0 +1,10 @@
+const images = document.querySelectorAll('.image-gallery img');
+let currentImageIndex = 0;
+
+function showNextImage() {
+  images[currentImageIndex].classList.remove('active');
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  images[currentImageIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 2000);
